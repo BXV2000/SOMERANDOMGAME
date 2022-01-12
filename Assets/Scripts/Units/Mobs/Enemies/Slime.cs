cc8a9214e9223bd23449c887d0be371150a2de3a
Mực <83195998+Muc2k1@users.noreply.github.com>
2022-01-12 20:57:40 +0700

    add slime movement

--- Assets/Scripts/Units/Mobs/Enemies/Slime.cs
+++ Assets/Scripts/Units/Mobs/Enemies/Slime.cs
@@ -4,9 +4,24 @@ using UnityEngine;
 
 public class Slime : Enemies
 {
+    private Vector2 randomPoint;
+
+    private void Start() 
+    {
+        InvokeRepeating("ChoseRandomDir", 0f, 3f);
+    }
+    private void Update() 
+    {
+        if(!target)
+            Move();
+        else
+            Chase();
+    }
+
+    #region Override
     override protected void Move()
     {
-        return;
+        transform.position += (transform.position - (Vector3)randomPoint).normalized * speed * Time.deltaTime;
     }
     override protected void Die()
     {
@@ -23,5 +38,23 @@ public class Slime : Enemies
     override protected void Attack() 
     {
         return;
+    }
+
+    #endregion
+    protected void Chase()
+    {
+        return;
+    }
+
+    private void ChoseRandomDir()
+    {
+        float randx = Random.Range(transform.position.x - 1f,transform.position.x + 1f);
+        float randy = Random.Range(transform.position.y - 1f,transform.position.y + 1f);
+        randomPoint = new Vector2(randx, randy);
+    }
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        // TakeDmg(other.gameObject.GetComponent<DamageableObjects>().dmg);
     }
 }
