8755a19c3ada0d3337f80ba60474d851e0edab1e
MucEned <tranhuutri0909@gmail.com>
2022-01-12 14:46:03 +0700

    add Items

---
+++ Assets/Scripts/Scriptables/Items/Equipments.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Items/Equipment")]
+public class Equipments : Items
+{
+    override protected void Use()
+    {
+        return;
+    }
+}
