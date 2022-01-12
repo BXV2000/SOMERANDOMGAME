96bc71862551d245321cc37b5f6a575b93143809
MucEned <tranhuutri0909@gmail.com>
2022-01-12 08:05:17 +0700

    basing project

---
+++ Assets/Scripts/Interfaces/IAutoSpawn.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IAutoSpawn
+{
+    void Remove();
+    void Spawn();
+}
---
+++ Assets/Scripts/Scriptables/Items.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Items : ScriptableObject
+{
+    protected int _id;
+    protected string _name;
+    protected bool stackAble;
+    protected string description;
+
+    protected abstract void Use();
+}
