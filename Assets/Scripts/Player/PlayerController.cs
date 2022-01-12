a83b34a0ec8ad39417bf3587af8e0ba7cedd8de7
MucEned <tranhuutri0909@gmail.com>
2022-01-12 15:32:34 +0700

    add player basic movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : Creatures
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,29 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        Vector2 moveAmount = moveInput.normalized * speed * Time.deltaTime;
+
+        transform.position += (Vector3)moveAmount;
+    }
+    override protected void Move()
+    {
+        return;
+    }
+    override protected void Die()
+    {
+        return;
+    }
+    override protected void TakeDmg(int dmg)
+    {
+        return;
+    }
+    override protected void HPEqual0() 
+    {
+        return;
+    }
+    override protected void Attack()
+    {
+        return;
     }
 }
