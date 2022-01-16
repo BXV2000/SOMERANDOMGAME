b4a5f3bb32f586cd192e8d35893b4ae02c716754
htdat2000 <12a6.ht.dat@gmail.com>
2022-01-16 09:29:30 +0700

    BasicGameEventManager
    manage and trigger events using ScriptableObject

---
+++ Assets/Scripts/Manager/GameEvent/EventListener.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class EventListener 
+{
+    [SerializeField] private string name;
+    [SerializeField] private GameEvent gameEvent;
+    public UnityEvent unityEvent; 
+    public void Register()
+    {
+        gameEvent.Register(this);
+    }
+    public void Deregister()
+    {
+        gameEvent.Deregister(this);
+    }
+    public void RaiseEvent()
+    {
+        unityEvent?.Invoke();
+    }
+}
---
+++ Assets/Scripts/Manager/GameEvent/EventManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour
+{
+    [SerializeField] private List<EventListener> listeners = new List<EventListener>();
+    private void Awake()
+    {
+        foreach (EventListener listener in listeners)
+        {
+            listener.Register();
+        }
+    }
+    private void OnDestroy()
+    {
+        foreach (EventListener listener in listeners)
+        {
+            listener.Deregister();
+        }
+    }
+}
